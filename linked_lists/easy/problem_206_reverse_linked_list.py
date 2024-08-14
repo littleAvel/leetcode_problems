5c77cc2fd24e7fb5501e70b8382aea78ff712fd7
Malika Umurzakova <malika@Malikas-MacBook-Pro.local>
2024-08-14 22:13:00 +0600

    Add easy problems on Linked Lists

---
+++ linked_lists/easy/problem_206_reverse_linked_list.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+
+        while head != None:
+            node = head
+            head = head.next
+            node.next = prev
+            prev = node
+
+        return prev
