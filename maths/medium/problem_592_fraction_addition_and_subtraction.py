2f6e3cb6000210264247f6fea829b32b64515d47
Malika Umurzakova <malika@Malikas-MacBook-Pro.local>
2024-08-23 17:27:08 +0600

    Update problem #592

--- maths/medium/problem_592_fraction_addition_and_subtraction.py
+++ maths/medium/problem_592_fraction_addition_and_subtraction.py
@@ -1,6 +1,6 @@
 from typing import List
 
-
+# Solution #1
 class Solution:
     def computeGCD(self, x: str, y: str) -> str:
         while(y):
@@ -42,3 +42,35 @@ class Solution:
         ans = f"{res // gcd_res}/{lcm // gcd_res}"
 
         return ans
+
+# Solution #2
+import re
+
+
+class Solution:
+    def fractionAddition(self, expression: str) -> str:
+        num = 0
+        denom = 1
+
+        # separate expression into signed numbers
+        nums = re.split("/|(?=[-+])", expression)
+        nums = list(filter(None, nums))
+
+        for i in range(0, len(nums), 2):
+            curr_num = int(nums[i])
+            curr_denom = int(nums[i + 1])
+
+            num = num * curr_denom + curr_num * denom
+            denom = denom * curr_denom
+
+        gcd = abs(self._find_gcd(num, denom))
+
+        num //= gcd
+        denom //= gcd
+
+        return str(num) + "/" + str(denom)
+
+    def _find_gcd(self, a: int, b: int) -> int:
+        if a == 0:
+            return b
+        return self._find_gcd(b % a, a)
